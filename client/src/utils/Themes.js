8df8c3caebbf907cda1722b844461695a2cce60c
amishana1k <amishanaik44033@gmail.com>
2024-11-20 23:13:34 +0530

    Added new folders for the Podify app

---
+++ client/src/utils/Themes.js
@@ -0,0 +1,20 @@
+export const darkTheme = {
+    bg:"#111111",
+    bgLight: "#000000",
+    primary:"#E50914",
+    text_primary:"#F2F3F4",
+    text_secondary:"#E50914",
+    card:"#121212",
+    button:"#5c5b5b",
+}
+// #E50914
+
+export const lightTheme = {
+    bg:"#FFFFFF",
+    bgLight: "#f0f0f0",
+    primary:"#E50914",
+    text_primary:"#111111",
+    text_secondary:"#48494a",
+    card:"#FFFFFF",
+    button:"#5c5b5b",
+}
